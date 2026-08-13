530e248bb34aed7aba4bbc1c6623dedef8753966
Nehhaaa02 <aapka-github-nehaaswal028@gmail.com>
2026-08-13 20:16:10 +0530

    fix profile API routes

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -38,7 +38,7 @@ function Profile() {
     e.preventDefault();
 
     try {
-      const response = await API.put("/users/profile", {
+      const response = await API.put("/api/users/profile", {
         fullName: formData.fullName,
         phone: formData.phone,
       });
